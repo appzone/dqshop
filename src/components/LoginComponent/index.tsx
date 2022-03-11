3572a2245d68c64ba4f48485793c7125ca1f06a5
DQ <dq@dq.com>
2022-03-11 18:16:20 +0700

    try update

--- src/components/LoginComponent/index.tsx
+++ src/components/LoginComponent/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const LoginComponent = (props:any) => {
 	return (
 		<button>
-			<a href="http://143.110.236.149:8180/auth/realms/DQLabLogin/protocol/openid-connect/auth?client_id=online-shop-application&response_type=code&redirect_uri=http://localhost:3000/callback">Sign in With DQ</a>
+			<a href="http://143.110.236.149:8180/auth/realms/DQLabLogin/protocol/openid-connect/auth?client_id=online-shop-application&response_type=code&redirect_uri=http://165.22.58.178:31989/callback">Sign in With DQ</a>
 		</button>
 	)
 }
